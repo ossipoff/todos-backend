1fb81c556af071ca486d7f17433dc4ac5b693471
Daniel Jensen <ossipoff@gmail.com>
2022-02-17 12:06:33 +0100

    fix: updated to net5.0

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using TodosBackend.EntityFramework;
+using Microsoft.Extensions.Hosting;
 
 namespace TodosBackend
 {
@@ -32,11 +26,10 @@ namespace TodosBackend
                 options.AddPolicy("CorsPolicy",
                     builder => builder.AllowAnyOrigin()
                     .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials());
+                    .AllowAnyHeader());
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
 
             var connection = "Data Source=todos.db";
             services.AddDbContext<TodosContext>
@@ -44,7 +37,7 @@ namespace TodosBackend
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -56,10 +49,15 @@ namespace TodosBackend
                 app.UseHsts();
             }
 
+            app.UseRouting();
+
             app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
